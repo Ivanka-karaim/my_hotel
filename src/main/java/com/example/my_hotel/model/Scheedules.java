c75c19b4d18854dfaac6d1305f89e3ff042fc9db
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2021-12-20 14:51:10 +0200

    Create Scheedules.java

---
+++ src/main/java/com/example/my_hotel/model/Scheedules.java
@@ -0,0 +1,31 @@
+package com.example.my_hotel.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Schedules {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date dateBegin;
+    
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date dateEnd;
+}
