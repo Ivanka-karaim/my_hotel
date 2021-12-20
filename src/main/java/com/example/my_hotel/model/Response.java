d5d786c80f6547f446285af372a8e057a88a6914
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2021-12-20 18:13:51 +0200

    new model

---
+++ src/main/java/com/example/my_hotel/model/Response.java
@@ -0,0 +1,29 @@
+package com.example.my_hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Response {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_response;
+
+    @NotNull
+    private int mark;
+
+    @NotNull
+    @Size(max=255)
+    private String comment;
+
+}
