fade80814a9b2eb100cd5299046e1c7517be269d
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2021-12-20 16:53:22 +0200

    Booking

--- src/main/java/com/example/my_hotel/controller/BookingController.java
+++ src/main/java/com/example/my_hotel/controller/BookingController.java
@@ -1,13 +1,42 @@
 package com.example.my_hotel.controller;
 
+import com.example.my_hotel.model.Booking;
+import com.example.my_hotel.repository.RoomRepository;
+import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Controller
 public class BookingController {
+    @Autowired
+    private RoomRepository roomRepository;
+
     @GetMapping("/booking")
     public String main(Model model) {
+        model.addAttribute("booking", new Booking());
+        model.addAttribute("error","");
         return "booking";
+    }
+    @PostMapping("/booking")
+    public String search(Booking booking, Model model) {
+        LocalDate date1 = LocalDate.ofInstant(booking.getDate_arrival().toInstant(), ZoneId.systemDefault());
+        LocalDate date2 = LocalDate.ofInstant(booking.getDate_departure().toInstant(), ZoneId.systemDefault());
+        if (date1.isAfter(date2)) {
+            model.addAttribute("error","Error");
+            return "booking";
+
+        }
+        else {
+            model.addAttribute("booking", booking);
+            return "booking/free_room";
+        }
     }
 }
--- src/main/java/com/example/my_hotel/model/Booking.java
+++ src/main/java/com/example/my_hotel/model/Booking.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.tomcat.jni.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Builder
