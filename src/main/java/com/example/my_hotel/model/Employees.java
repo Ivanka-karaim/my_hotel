4dd00cf3589eba0def91a0b112487d77d9c46273
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2021-12-20 14:46:42 +0200

    Update Employees.java

--- src/main/java/com/example/my_hotel/model/Employees.java
+++ src/main/java/com/example/my_hotel/model/Employees.java
@@ -3,10 +3,34 @@ package com.example.my_hotel.model;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Employees {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_employee;
-    private String surname;
+    
+    @NotNull
+    @Size(max = 50)
+    private String surname;    
+    @NotNull
+    @Size(max = 50)
+    private String fullname;    
+    @NotNull
+    @Size(max = 50)
+    private String patron;
+    
+    @NotNull
+    private int ipn;
 }
