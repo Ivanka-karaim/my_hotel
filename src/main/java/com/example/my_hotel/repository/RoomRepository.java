f6902636d8312b85a840ba43f3a124aa08750286
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2021-12-22 00:04:58 +0200

    room

--- src/main/java/com/example/my_hotel/repository/RoomRepository.java
+++ src/main/java/com/example/my_hotel/repository/RoomRepository.java
@@ -15,8 +15,8 @@ public interface RoomRepository extends CrudRepository<Room, Integer> {
     List<Room> findAll();
     List<Room> findAllByFloor(int floor);
     Optional<Room> findById(int id);
-//    @Query("SELECT r FROM room r WHERE id_room != ALL(SELECT id_room  FROM booking WHERE date_arrival<:date1 AND date_departure>:date2) " +
-//           "AND id_classification = ANY(SELECT id_classification FROM classificationroom WHERE number_beds=:number)")
-//    List<Room> testQueryAnnotationParams(@Param("date1") Date date1, @Param("date2") Date date2, @Param("number") int number);
+    @Query("SELECT r FROM Room r WHERE r.id_room != ALL(SELECT b.room FROM Booking b WHERE b.date_arrival<:date1 AND b.date_departure>:date2) " +
+           "AND id_classification = ANY(SELECT id_classification FROM Classificationroom WHERE number_beds=:number)")
+    List<Room> my_func(@Param("date1") String date1, @Param("date2") String date2, @Param("number") int number);
 
 }
