255005e1c2da02220eb622666483bf70373f00f1
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2021-12-22 00:57:36 +0200

    new changes

--- src/main/java/com/example/my_hotel/model/Inhabitant.java
+++ src/main/java/com/example/my_hotel/model/Inhabitant.java
@@ -1,37 +1,37 @@
-//package com.example.my_hotel.model;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import javax.persistence.*;
-//import javax.validation.constraints.NotNull;
-//import javax.validation.constraints.Size;
-//import java.time.LocalDate;
-//
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Entity
-//public class Inhabitant {
-//    @Id
-//    @Size(max = 13)
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private String phone_number;
-//
-//    @NotNull
-//    @Size(max = 20)
-//    private String surname;
-//
-//    @NotNull
-//    @Size(max = 20)
-//    private String name;
-//
-//    @NotNull
-//    @Size(max = 20)
-//    private String patronymic;
-//
-//    @NotNull
-//    private LocalDate birthday;
-//}
+package com.example.my_hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Inhabitant {
+    @Id
+    @Size(max = 13)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String phone_number;
+
+    @NotNull
+    @Size(max = 20)
+    private String surname;
+
+    @NotNull
+    @Size(max = 20)
+    private String name;
+
+    @NotNull
+    @Size(max = 20)
+    private String patronymic;
+
+    @NotNull
+    private LocalDate birthday;
+}
--- src/main/java/com/example/my_hotel/model/Order_Inhabitant.java
+++ src/main/java/com/example/my_hotel/model/Order_Inhabitant.java
@@ -1,27 +1,29 @@
-//package com.example.my_hotel.model;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import javax.persistence.*;
-//import javax.validation.constraints.NotNull;
-//import javax.validation.constraints.Size;
-//
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Entity
-//public class Order_Inhabitant {
-//    @Id
-//    @ManyToOne
-//    @Size(max = 13)
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private String phone_number;
-//
-//    @ManyToOne
-//    @NotNull
-//    private int id_order;
-//
-//}
+package com.example.my_hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Order_Inhabitant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_order_inhabitant;
+
+    @ManyToOne
+    @JoinColumn(name ="phone_number")
+    private Inhabitant phone_number;
+
+    @OneToOne
+    @JoinColumn(name ="id_order")
+    private Custom id_order;
+
+}
