7d272747970fae2720d01253d7cc9b56a0d9c514
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2021-12-21 23:31:59 +0200

    booking

--- src/main/java/com/example/my_hotel/controller/BookingController.java
+++ src/main/java/com/example/my_hotel/controller/BookingController.java
@@ -7,22 +7,25 @@ import com.example.my_hotel.repository.RoomRepository;
 import com.example.my_hotel.service.RoomService;
 import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BookingController {
     @Autowired
     private RoomService roomService;
+    private Date date_1;
+    private Date date_2;
+    private int count_1;
 
 
     @GetMapping("/booking")
@@ -37,18 +40,34 @@ public class BookingController {
         LocalDate date2 = LocalDate.ofInstant(booking.getDate_departure().toInstant(), ZoneId.systemDefault());
         System.out.println(date1);
         System.out.println(date2);
+        date_1=booking.getDate_arrival();
+        date_2=booking.getDate_departure();
+        count_1=booking.getCount_people();
         if (date1.isAfter(date2)) {
             model.addAttribute("error","Error");
             return "booking";
-
         }
         else {
             model.addAttribute("booking", booking);
-
             List<RoomDTO> rooms = roomService.getAllRooms();
-
             model.addAttribute("rooms", rooms);
             return "booking/free_room";
         }
+    }
+    @GetMapping("/booking/{id}")
+    public String booking_custom(@PathVariable(value="id") int id,   Model model) {
+        Booking booking = new Booking();
+        booking.setRoom(roomService.getById(id));
+        booking.setDate_arrival(date_1);
+        booking.setDate_departure(date_2);
+        booking.setCount_people(count_1);
+        System.out.println(booking.getDate_arrival());
+        model.addAttribute("booking", booking);
+        return "booking/form";
+    }
+    @PostMapping("/booking/{id}")
+    public String booking_rest(Booking booking, Model model)  {
+        System.out.println(booking.getName());
+        return "general";
     }
 }
