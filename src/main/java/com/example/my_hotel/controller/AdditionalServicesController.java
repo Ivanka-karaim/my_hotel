b50f17e4e2673d86058ac8edf5dbadb9b49fbef7
Victoria Fox <83537604+holyviktor@users.noreply.github.com>
2021-12-22 14:14:19 +0200

    new page with additional services

---
+++ src/main/java/com/example/my_hotel/controller/AdditionalServicesController.java
@@ -0,0 +1,62 @@
+package com.example.my_hotel.controller;
+
+import com.example.my_hotel.dto.AdditionalServicesDTO;
+import com.example.my_hotel.dto.CustomDTO;
+import com.example.my_hotel.dto.RoomDTO;
+import com.example.my_hotel.model.AdditionalServices;
+import com.example.my_hotel.model.Booking;
+import com.example.my_hotel.model.Custom;
+import com.example.my_hotel.model.Room;
+import com.example.my_hotel.repository.RoomRepository;
+import com.example.my_hotel.service.AdditionalServicesService;
+import com.example.my_hotel.service.CustomService;
+import com.example.my_hotel.service.RoomService;
+import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class AdditionalServicesController {
+    @Autowired
+    private AdditionalServicesService additionalServicesService;
+
+    @GetMapping("/viewServ")
+    public String view(Model model) {
+        List<AdditionalServicesDTO> additionalServices = additionalServicesService.getAllAdditionalServices();
+        model.addAttribute("title", "View");
+        model.addAttribute("page", additionalServices);
+        return "additionalservices";
+    }
+
+    @GetMapping("/addServ")
+    public String add(Model model) {
+        List<AdditionalServicesDTO> additionalServices = additionalServicesService.getAllAdditionalServices();
+        model.addAttribute("title", "Add");
+        return "additionalservices";
+    }
+
+    @GetMapping("/removeServ")
+    public String remove(Model model) {
+        List<AdditionalServicesDTO> additionalServices = additionalServicesService.getAllAdditionalServices();
+        model.addAttribute("title", "Remove");
+        return "additionalservices";
+    }
+
+    @GetMapping("/editServ")
+    public String edit (Model model) {
+        List<AdditionalServicesDTO> additionalServices = additionalServicesService.getAllAdditionalServices();
+        model.addAttribute("title", "Edit");
+        return "additionalservices";
+    }
+}
+
