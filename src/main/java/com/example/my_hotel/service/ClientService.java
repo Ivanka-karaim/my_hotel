2ade1698517f2813b63beee94552ab48e309d6e3
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2021-12-24 03:35:40 +0200

    new

--- src/main/java/com/example/my_hotel/service/ClientService.java
+++ src/main/java/com/example/my_hotel/service/ClientService.java
@@ -1,4 +1,13 @@
 package com.example.my_hotel.service;
 
+import com.example.my_hotel.model.Booking;
+import com.example.my_hotel.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientService {
-}
+    @Autowired
+    private BookingRepository bookingRepository;
+
